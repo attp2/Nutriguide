f266dfe515c586f5d9711cb4c7b2309e386b7532
attp2 <97297007+attp2@users.noreply.github.com>
2026-05-29 09:06:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriguide-v15';
+const CACHE = 'nutriguide-v16';
 const FILES = ['/', '/index.html', '/data.js', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/glossaire/glossaire.js'];
 
 self.addEventListener('install', e => {
